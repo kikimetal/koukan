0f668dc3765ac95b670a5ed0834b924a4de9834f
kentarohorie <kentaro.horie.4@gmail.com>
2017-10-11 14:18:04 +0900

    update config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'top#index'
   get 'admin' => 'admin/reports#index'
+
+  resources :countries, only: [:index, :show]
+  resources :abroad_universities, only: :show
+  resources :reports, only: :show
+
   namespace :admin do
     resources :reports
   end
